30a1e5a806e25819360392796da82e29df20be4c
sidakinaddd <sidakinaddd@gmail.com>
2020-04-15 02:44:37 +0600

    viewsets

--- week6/todo/main/urls.py
+++ week6/todo/main/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from rest_framework import routers
+
 from .views import ToDoListView, ToDoListsView,ToDosView, ToDoView
 from .viewscol.views import ToDoListCBV, ToDoListsCBV
 from .viewscol import views
@@ -6,18 +8,16 @@ from .viewscol.mixins import TodoListsView
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
+router = routers.DefaultRouter()
+router.register('mixin', TodoListsView, basename='api')
 
 urlpatterns = [
     path('', ToDoListsView.as_view()),
     path('<int:pk>/', ToDoListView.as_view()),
     path('<int:pk>/todo/', ToDosView.as_view()),
     path('<int:pk>/todo/<int:pk2>/', ToDoView.as_view()),
-    path('cbv/', views.ToDoListsCBV.as_view()),
-    path('cbv/<int:pk>/', views.ToDoListsCBV.as_view()),
-    path('mix/', TodoListsView.as_view()),
-
-
-
+    # path('cbv/', views.ToDoListsCBV.as_view()),
+    # path('cbv/<int:pk>/', views.ToDoListsCBV.as_view()),
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = format_suffix_patterns(urlpatterns) + router.urls
--- week6/todo/main/viewscol/mixins.py
+++ week6/todo/main/viewscol/mixins.py
@@ -1,20 +1,23 @@
 from rest_framework import generics
 from rest_framework import mixins, viewsets
 from ..serializers import TodoListSerializer
-from ..models import ToDoList,ToDo
+from ..models import ToDoList, ToDo
+
 from rest_framework.permissions import IsAuthenticated
 
-class TodoListsView(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
-    queryset = ToDoList.objects.all()
+
+class TodoListsView(mixins.RetrieveModelMixin,
+                    mixins.ListModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    mixins.CreateModelMixin,
+                    viewsets.GenericViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = TodoListSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+    def get_queryset(self):
+        return ToDoList.objects.for_user(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
